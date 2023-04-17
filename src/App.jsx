4c064916c179e6f4cf2709414773b994989f73fe
name <singhshiv0427@gmail.com>
2023-04-17 23:21:50 +0530

    first comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-
+import "./app.css"
 
 function App() {
   return (
-    <div className="App">
-      Quiz App
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  );
+  );   
 }
 
 export default App;

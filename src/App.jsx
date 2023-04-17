8cfcd19c85b960ec2f0092b459a3065949e0e8d5
name <singhshiv0427@gmail.com>
2023-04-18 02:24:53 +0530

    second comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import "./app.css"
+import Trivia from "./components/Trivia";
 
 function App() {
+    const  [questionNumber, setQuestionNumber] = useState(1);
+    const moneyPyramid = [
+      { id: 15, amount: "$1000000" },
+      { id: 14, amount: "$500000" },
+      { id: 13, amount: "$200000" },
+      { id: 12, amount: "$100000" },
+      { id: 11, amount: "$50000" },
+      { id: 10, amount: "$20000" },
+      { id: 9, amount: "$10000" },
+      { id: 8, amount: "$5000" },
+      { id: 7, amount: "$1000" },
+      { id: 7, amount: "$1000" },
+      { id: 6, amount: "$500" },
+      { id: 5, amount: "$1000" },
+      { id: 4, amount: "$500" },
+      { id: 3, amount: "$300" },
+      { id: 2, amount: "$200" },
+      { id: 1, amount: "$100" },
+    ];
+
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+            <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+            <Trivia />
+        </div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
-          <li className="moneyListItem active">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">$ 400</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">$ 400</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">$ 400</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">$ 400</span>
-          </li>
+          {moneyPyramid.map((m) => (
+            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,16 @@
+
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">
+        The International Literacy day is observed on?
+      </div>
+      <div className="answers">
+        <div className="answer correct">Sep 8</div>
+        <div className="answer">Nov 28</div>
+        <div className="answer">May 2</div>
+        <div className="answer">Sep 22</div>
+      </div>
+    </div>
+  );
+}
